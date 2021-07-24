e07792b074f8c5b456dd379b9b3c1ef53cd22f92
budds300 <budds300@gmail.com>
2021-07-24 10:59:30 +0300

    updated views for initial test

--- estate/views.py
+++ estate/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse,HttpResponseRedirect,Http404,JsonResponse
+import datetime as dt
+from django.contrib import messages
+
+
 
 # Create your views here.
+def home(request):
+    return render(request,'estate/home.html')
