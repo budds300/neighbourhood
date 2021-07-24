7d07da5a51032631dd77be27cd699c858772c0b4
budds300 <budds300@gmail.com>
2021-07-24 10:58:03 +0300

    updated user application model app with a profile model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+from django.db.models.fields import TextField
+from estate.models import Neighbourhood
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    image=CloudinaryField('profile-photo',null=True, transformation=[{'width':300, 'height':300}])
+    bio=models.TextField(null=True)
+    neighourhood = models.ForeignKey(Neighbourhood,on_delete=models.CASCADE,null=True,blank=True)
+
+    
+    def __str__(self):
+        return f'{self.user.username} Profile'
