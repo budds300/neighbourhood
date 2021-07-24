848f39fae3dbaafd231aa650c7fa885a39f6ab37
budds300 <budds300@gmail.com>
2021-07-24 11:48:59 +0300

    importation of listview,detailView createView,updateView and DeleteView

--- estate/views.py
+++ estate/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse,HttpResponseRedirect,Http404,JsonResponse
 import datetime as dt
 from django.contrib import messages
-
+from django.contrib.auth.decorators import login_required
+from django.views.generic import ListView,DetailView,CreateView,UpdateView,DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
 
 
 # Create your views here.
