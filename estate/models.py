ac6d4e8666c68d0b05628b7e7d9517b834a44563
budds300 <budds300@gmail.com>
2021-07-26 01:42:46 +0300

    corrected typo on neighborhood

--- estate/models.py
+++ estate/models.py
@@ -1,3 +1,4 @@
+import sys
 from django.db import models
 from cloudinary.models import CloudinaryField
 from django.contrib.auth.models import User
@@ -5,20 +6,20 @@ from django.db.models.fields import TextField
 from django.urls import reverse
 # Create your models here.
 
-class Neighbourhood(models.Model):
+class Neighborhood(models.Model):
     neighborhood_name = models.CharField(max_length=100)
     location = models.CharField(max_length=100,null=True)
     occupant_count = models.IntegerField(null=True)
     
-    def save_neighbourhood(self):
+    def save_neighborhood(self):
         self.save()
         
     @classmethod
-    def delete_neighbourhood(cls,id):
+    def delete_neighborhood(cls,id):
         cls.objects.filter(id).delete()
 
     @classmethod
-    def update_neighbourhood(cls,id,new_name):
+    def update_neighborhood(cls,id,new_name):
         cls.objects.filter(id=id).update(hood_name = newname)
    
     @classmethod
@@ -27,25 +28,25 @@ class Neighbourhood(models.Model):
 
     @classmethod
     def search_hood(cls,id):
-       search= cls.objects.filter(neighbourhood_name__icontains=id)
+       search= cls.objects.filter(neighborhood_name__icontains=id)
        return search
     
     
    
 class Business(models.Model):
     name=models.CharField(max_length=100)
-    business_owner=models.CharField(max_length=100)
+    business_owner=models.ForeignKey(User,on_delete=models.CASCADE,null=True)
     email=models.EmailField()
     business_image= CloudinaryField('business', null=True)
-    business_location=models.ForeignKey(Neighbourhood,on_delete=models.CASCADE,null=True)
-    location=models.CharField(max_length=100,null=True,blank=True)
+    business_location=models.ForeignKey(Neighborhood,on_delete=models.CASCADE,null=True)
+    location=models.CharField(max_length=225,null=True,blank=True)
 
-    def save(self):
+    def save_business(self):
         self.save()
     
     @classmethod
     def delete_business(id,cls):
-        cls.objects.filter(id=id).delete()
+        cls.objects.filter(id).delete()
     
     @classmethod
     def update_business(cls,id,new_name):
@@ -66,7 +67,7 @@ class Post(models.Model):
     author = models.ForeignKey(User,on_delete=models.CASCADE,null=True)
     post = TextField(null=True)
     date_posted = models.DateTimeField(auto_now=True)
-    neighourhood = models.ForeignKey(Neighbourhood,on_delete=models.CASCADE,null=True,blank=True)
+    neighourhood = models.ForeignKey(Neighborhood,on_delete=models.CASCADE,null=True,blank=True)
 
     
     def get_absolute_url(self):
